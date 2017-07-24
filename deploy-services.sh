812ca560e9e61bf85d79bcdf45064a1267289dfd
Peter DeMartini <thepeterdemartini@gmail.com>
2017-07-24 10:04:34 -0700

    added deploy-services script

---
+++ deploy-services.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+set -e
+
+export BEEKEEPER_URI='https://hive-manager:some-random-password@beekeeper.nodesummit.octoblu.com'
+
+ensure_beekeeper_util() {
+  if [ -z "$(which beekeeper)" ]; then
+    if [ -z "$(which yarn)" ]; then
+      echo '* running "yarn global add beekeeper-util"'
+      yarn global add beekeeper-util
+    else
+      echo '* running "npm install --global beekeeper-util"'
+      npm install --global beekeeper-util
+    fi
+  fi
+}
+
+create_deployment() {
+  local owner="$1"
+  local repo="$2"
+  local tag="$3"
+  echo "* deploying $owner/$repo:$tag"
+  curl --silent --fail -X POST "$BEEKEEPER_URI/deployments/$owner/$repo/$tag"
+}
+
+mark_deployment_as_succesful() {
+  local owner="$1"
+  local repo="$2"
+  local tag="$3"
+  echo "* marking $owner/$repo:$tag as successful"
+  beekeeper webhook \
+    --type codefresh \
+    --ci-passing true \
+    --tag "$tag" \
+    --owner "$owner" \
+    "$repo"
+}
+
+main() {
+  ensure_beekeeper_util
+  create_deployment "octoblu" "beekeeper-service" "v4.0.0"
+  mark_deployment_as_succesful "octoblu" "beekeeper-service" "v4.0.0"
+  create_deployment "octoblu" "beekeeper-worker" "v2.0.1"
+  mark_deployment_as_succesful "octoblu" "beekeeper-worker" "v2.0.1"
+  create_deployment "octoblu" "beekeeper-updater-swarm" "v2.2.3"
+  mark_deployment_as_succesful "octoblu" "beekeeper-updater-swarm" "v2.2.3"
+  create_deployment "octoblu" "server-status-code" "v2.0.2"
+  mark_deployment_as_succesful "octoblu" "server-status-code" "v2.0.2"
+}
+
+main "$@"
